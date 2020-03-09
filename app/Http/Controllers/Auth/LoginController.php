8f78461bbf2e2de01dee4dbd2f1300041ddd08e5
rodAlfaro-98 <rodrigoalfarod@gmail.com>
2020-03-09 14:34:33 -0600

    Proyecto generado hasta la fecha 9/03/2020, funciona todo menos el redireccionamiento posterior a la autentificación con llaves de servicios externos

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Socialite;
+use Auth;
+use App\User;
+//use Laravel\Socialite\Facades\Socialite;
+
+function credentials(Request $request)
+{
+    $request['active'] = 1;
+    return $request->only($this->username(), 'password', 'active');
+}
+
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+
+
+
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->stateless()->user();
+        return $user->token;
+    }
+
+}
